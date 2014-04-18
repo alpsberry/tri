03615cbe5ea9698cef5957e981a799c980b05cc8
alpsberry <xiaolinguo.nju@gmail.com>
2014-04-18 18:32:07 +0800

    added a Discontinuous Galerkin solver

---
+++ tri_DG.cpp
@@ -0,0 +1,18 @@
+#include "tri_DG.h"
+
+int main(int argc, char const *argv[])
+{
+	MyProblem prob;
+	if(prob.initProblem(argc, argv) == 1)
+		return 1;
+
+	Mesh mesh;
+	if(mesh.initMesh(prob) == 1)
+		return 1;
+
+	DGSolvingSystem solSys;
+	solSys.assembleStiff(mesh, prob);
+	solSys.solveSparse(mesh, prob);
+	solSys.triOutput(prob, mesh);
+	return 0;
+}
---
+++ tri_DG.h
@@ -0,0 +1,11 @@
+#ifndef TRI_H
+#define TRI_H
+
+// #define __TRI_DEBUG
+
+#include "mesh.h"
+#include "solveSys.h"
+#include "DGSolveSys.h"
+#include "problem.h"
+
+#endif
