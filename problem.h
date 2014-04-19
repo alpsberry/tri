ef9f336f3df853d3dc0f1a3e21422e77f1ff760f
alpsberry <xiaolinguo.nju@gmail.com>
2014-04-19 23:29:50 +0800

    seperate Problem class and MyProblem class, change the latter to DGProblem and FEMProblem

--- problem.h
+++ problem.h
@@ -25,10 +25,11 @@ struct Edge{
 struct Element{
 	double detBE;
 	int index;
+	int localDof;
 	std::vector<int> vertex;
 	std::vector<int> edge;
 };
-// detBE is the absolute value of the determinant of B_E
+// detBE is the determinant of B_E
 // The mapping F_E from the reference element to any element E is
 // F([x, y]^T) = B_E[x, y]^T + b_E
 // where B_E = [x2 - x1, x3 - x1; y2 - y1. y3 - y1] and b_E = [x1, y1]^T
@@ -59,6 +60,9 @@ public:
 	// Region region;
 	paramstruct parameters;
 	int dimension;
+	double epsilon;
+	double sigma0;
+	double beta0;
 
 	virtual double f(double x, double y){
 		return 0;
@@ -69,19 +73,6 @@ public:
 		return 0;
 	}
 
-	// read parameters from an input file
-	int initProblem(int argc, char const *argv[]);
-};
-
-// the problem to be solved here is
-// -\Delta u + u = 3\cos x \sin y, (x,y) \in \Omega = [\frac{\pi}{2}, \frac{3\pi}{2}] \times [0, \pi]
-// u = 0, (x,y) \in \Gamma
-class MyProblem: public Problem
-{
-public:
-	double f(double x, double y){
-		return 3 * cos(x) * sin(y);
-	}
 };
 
 #endif
