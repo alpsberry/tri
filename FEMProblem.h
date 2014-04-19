6d6815fdcb21193e57367f06f780b7c519db8b3f
alpsberry <xiaolinguo.nju@gmail.com>
2014-04-19 23:23:46 +0800

    define problem parameters for the FEM solving system

---
+++ FEMProblem.h
@@ -0,0 +1,21 @@
+#ifndef TRI_FEMPROBLEM_H
+#define TRI_FEMPROBLEM_H
+
+#include <iostream>
+#include "problem.h"
+
+// the problem to be solved here is
+// -\Delta u + u = 3\cos x \sin y, (x,y) \in \Omega = [\frac{\pi}{2}, \frac{3\pi}{2}] \times [0, \pi]
+// u = 0, (x,y) \in \Gamma
+class FEMProblem: public Problem
+{
+public:
+	double f(double x, double y){
+		return 3 * cos(x) * sin(y);
+		// return 1.0;
+	}
+	// read parameters from an input file
+	int initProblem(int argc, char const *argv[]);
+};
+
+#endif
