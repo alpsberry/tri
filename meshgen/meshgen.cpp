27a4a3a80fdee4cfe84a503ee16073f11601cf21
alpsberry <xiaolinguo.nju@gmail.com>
2014-05-21 14:24:30 +0800

    fix a bug in vertex output

--- meshgen/meshgen.cpp
+++ meshgen/meshgen.cpp
@@ -116,7 +116,7 @@ void outputEdge()
 
 }
 
-void outputRefine()
+void outputRefineLeft()
 {
 	ofstream fout((filename + ".1.ref0").c_str());
 
@@ -125,7 +125,7 @@ void outputRefine()
 	fout << 4 * N - 3 << endl;
 
 	fout << "   " << originNodeSize + 1 << "\t" << Left + 0.5 * hx << "\t" << Bottom << "\t1" << endl;
-	fout << "   " << originNodeSize + 2 << "\t" << Left << "\t" << Bottom + 0.5 * hy << "\t0" << endl;
+	fout << "   " << originNodeSize + 2 << "\t" << Left << "\t" << Bottom + 0.5 * hy << "\t1" << endl;
 	fout << "   " << originNodeSize + 3 << "\t" << Left + 0.5 * hx << "\t" << Bottom + 0.5 * hy << "\t0" << endl;
 	fout << "   " << originNodeSize + 4 << "\t" << Left + hx << "\t" << Bottom + 0.5 * hy << "\t0" << endl;
 	for(int i = 2; i <= N - 1; i++)
@@ -202,8 +202,107 @@ void outputRefine()
 		     << "\t\t" << originElementSize + 8 * (j - 1) + 8 << "\t" << originNodeSize + 4 * (j - 1) + 3 << "\t" << originNodeSize + 4 * (j - 1) + 4 << "\t" << j * N + 2 << endl;
 	}
 
+}
+
+void outputRefineCentral0()
+{
+	ofstream fout((filename + ".1.ref0").c_str());
+
+	const double startx = N / 2;
+	const double starty = N / 2;
+
+	// node
+	const int originNodeSize = N * N;
+	fout << 5 << endl;
+	fout << "   " << originNodeSize + 1 << "\t" << Left + (startx - 1) * hx + 0.5 * hx << "\t" << Bottom + (starty - 1) * hy << "\t0"<< endl
+	     << "   " << originNodeSize + 2 << "\t" << Left + (startx - 1) * hx << "\t" << Bottom + (starty - 1) * hy + 0.5 * hy << "\t0"<< endl
+	     << "   " << originNodeSize + 3 << "\t" << Left + (startx - 1) * hx + 0.5 * hx << "\t" << Bottom + (starty - 1) * hy + 0.5 * hy << "\t0"<< endl
+	     << "   " << originNodeSize + 4 << "\t" << Left + startx * hx << "\t" << Bottom + (starty - 1) * hy + 0.5 * hy << "\t0"<< endl
+	     << "   " << originNodeSize + 5 << "\t" << Left + (startx - 1) * hx + 0.5 * hx << "\t" << Bottom + starty * hy << "\t0"<< endl;
+
+	// edge
+	const int originEdgeSize = (N - 1) * (3 * N - 1);
+	fout << 16 << endl;
+
+	// edge row
+	fout << "\t" << (starty - 1) * (N - 1) + startx << endl
+	     << "\t\t" << originEdgeSize + 1 << "\t" << (starty - 1) * N + startx << "\t" << originNodeSize + 1 << endl
+	     << "\t\t" << originEdgeSize + 2 << "\t" << originNodeSize + 1 << "\t" << (starty - 1) * N + startx + 1 << endl
+	     << "\t0" << endl
+	     << "\t\t" << originEdgeSize + 3 << "\t" << originNodeSize + 2 << "\t" << originNodeSize + 3 << endl
+	     << "\t\t" << originEdgeSize + 4 << "\t" << originNodeSize + 3 << "\t" << originNodeSize + 4 << endl
+	     << "\t" << starty * (N - 1) + startx << endl
+	     << "\t\t" << originEdgeSize + 5 << "\t" << starty * N + startx << "\t" << originNodeSize + 5 << endl
+	     << "\t\t" << originEdgeSize + 6 << "\t" << originNodeSize + 5 << "\t" << starty * N + startx + 1 << endl;
+
+	// edge col
+	const int newRowSize = 6;
+	fout << "\t" << N * (N - 1) + (startx - 1) * (N - 1) + starty << endl
+	     << "\t\t" << originEdgeSize + newRowSize + 1 << "\t" << (starty - 1) * N + startx << "\t" << originNodeSize + 2 << endl
+	     << "\t\t" << originEdgeSize + newRowSize + 2 << "\t" << originNodeSize + 2 << "\t" << starty * N + startx << endl
+	     << "\t0" << endl
+	     << "\t\t" << originEdgeSize + newRowSize + 3 << "\t" << originNodeSize + 1 << "\t" << originNodeSize + 3 << endl
+	     << "\t\t" << originEdgeSize + newRowSize + 4 << "\t" << originNodeSize + 3 << "\t" << originNodeSize + 5 << endl
+	     << "\t" << N * (N - 1) + startx * (N - 1) + starty << endl
+	     << "\t\t" << originEdgeSize + newRowSize + 5 << "\t" << (starty - 1) * N + startx + 1 << "\t" << originNodeSize + 4 << endl
+	     << "\t\t" << originEdgeSize + newRowSize + 6 << "\t" << originNodeSize + 4 << "\t" << starty * N + startx + 1 << endl;	
+
+	// edge diag
+	const int newColSize = 6;
+	fout << "\t" << 2 * N * (N - 1) + (starty - 1) * (N - 1) + startx << endl
+	     << "\t\t" << originEdgeSize + newRowSize + newColSize + 1 << "\t" << (starty - 1) * N + startx << "\t" << originNodeSize + 3 << endl
+	     << "\t0" << endl
+	     << "\t\t" << originEdgeSize + newRowSize + newColSize + 2 << "\t" << originNodeSize + 1 << "\t" << originNodeSize + 4 << endl
+	     << "\t\t" << originEdgeSize + newRowSize + newColSize + 3 << "\t" << originNodeSize + 2 << "\t" << originNodeSize + 5 << endl
+	     << "\t" << 2 * N * (N - 1) + (starty - 1) * (N - 1) + startx << endl
+	     << "\t\t" << originEdgeSize + newRowSize + newColSize + 4 << "\t" << originNodeSize + 3 << "\t" << starty * N + startx + 1 << endl;
+
+
+	// element
+	const int originElementSize = 2 * (N - 1) * (N - 1);
+	fout << 8 << endl
+	     << "\t" << (starty - 1) * 2 * (N - 1) + 2 * (startx - 1) + 1 << endl
+	     << "\t\t" << originElementSize + 1 << "\t" << (starty - 1) * N + startx << "\t" << originNodeSize + 3 << "\t" << originNodeSize + 2 << endl
+	     << "\t" << (starty - 1) * 2 * (N - 1) + 2 * (startx - 1) + 2<< endl
+	     << "\t\t" << originElementSize + 2 << "\t" << (starty - 1) * N + startx << "\t" << originNodeSize + 1 << "\t" << originNodeSize + 3 << endl
+	     << "\t\t" << originElementSize + 3 << "\t" << originNodeSize + 1 << "\t" << originNodeSize + 4 << "\t" << originNodeSize + 3 << endl
+	     << "\t\t" << originElementSize + 4 << "\t" << originNodeSize + 1 << "\t" << (starty - 1) * N + startx + 1 << "\t" << originNodeSize + 4 << endl
+	     << "\t" << (starty - 1) * 2 * (N - 1) + 2 * (startx - 1) + 1<< endl
+	     << "\t\t" << originElementSize + 5 << "\t" << originNodeSize + 2 << "\t" << originNodeSize + 5 << "\t" << starty * N + startx << endl
+	     << "\t\t" << originElementSize + 6 << "\t" << originNodeSize + 2 << "\t" << originNodeSize + 3 << "\t" << originNodeSize + 5 << endl
+	     << "\t\t" << originElementSize + 7 << "\t" << originNodeSize + 3 << "\t" << starty * N + startx + 1 << "\t" << originNodeSize + 5 << endl
+	     << "\t" << (starty - 1) * 2 * (N - 1) + 2 * (startx - 1) + 2<< endl
+	     << "\t\t" << originElementSize + 8 << "\t" << originNodeSize + 3 << "\t" << originNodeSize + 4 << "\t" << starty * N + startx + 1 << endl;
+}
+
+void outputRefineCentral1()
+{
+	ofstream fout((filename + ".1.ref1").c_str());
+
+	const double startx = N / 2;
+	const double starty = N / 2;	
+	
+	// node
+	const int originNodeSize = N * N + 5;
+	fout << 1 << endl;
+	fout << "   " << originNodeSize + 1 << "\t" << Left + (startx - 1) * hx + 0.25 * hx << "\t" << Bottom + (starty - 1) * hy + 0.25 * hy << "\t0"<< endl;
+
+	// edge
+	const int originEdgeSize = (N - 1) * (3 * N - 1) + 16;
+	fout << 2 << endl
+	     << "\t" << originEdgeSize - 3 << endl
+	     << "\t\t" << originEdgeSize + 1 << "\t" << (starty - 1) * N + startx << "\t" << originNodeSize + 1 << endl
+	     << "\t\t" << originEdgeSize + 2 << "\t" << originNodeSize + 1 << "\t" << originNodeSize - 2 << endl;
+
+	// element
+	const int originElementSize = 2 * (N - 1) * (N - 1) + 8;
+	fout << 2 << endl
+	     << "\t" << originElementSize - 6 << endl
+	     << "\t\t" << originElementSize + 1 << "\t" << (starty - 1) * N + startx << "\t" << originNodeSize - 4 << "\t" << originNodeSize + 1 << endl
+	     << "\t\t" << originElementSize + 2 << "\t" << originNodeSize - 2 << "\t" << originNodeSize - 4 << "\t" << originNodeSize + 1 << endl;
 
 }
+
 int main(int argc, char **argv)
 {	
 	if( argc > 2 )
@@ -212,8 +311,10 @@ int main(int argc, char **argv)
 	N = atoi(argv[1]);      /* Number of processors requested */
 	cout << "N = " << N << endl;
 
-	if(N < 2)
+	if(N < 3){
+		cout << "N cannot be less than 3" << endl;
 		return 1;
+	}
 
 	hx = Width / (N - 1);
 	hy = Height / (N - 1);
@@ -222,5 +323,7 @@ int main(int argc, char **argv)
 	outputNode();
 	outputElement();
 	outputEdge();
-	outputRefine();
+	outputRefineLeft();
+	// outputRefineCentral0();
+	// outputRefineCentral1();
 }
