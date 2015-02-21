9dac4b63030de7e4acf0450d0fd394a8c74b587b
alpsberry <xiaolinguo.nju@gmail.com>
2015-02-21 14:25:50 +0100

    the new separated UMFPACK solver

---
+++ UMFPACKSolver.cpp
@@ -0,0 +1,30 @@
+//
+//  UMFPACKSolver.cpp
+//  tri
+//
+//  Created by GBB on 21/2/15.
+//  Copyright (c) 2014 Xiaolin Guo. All rights reserved.
+//
+
+#include "UMFPACKSolver.h"
+
+std::vector<double> UMFPACKSolver::solveSparse()
+{
+    std::cout << "start solving with UMFPACK" << std::endl;
+    double* x = new double [dof];
+    memset(x, 0, dof * sizeof(double));
+    void *Symbolic, *Numeric ;
+    (void) umfpack_di_symbolic (dof, dof, Ap, Ai, Ax, &Symbolic, NULL, NULL) ;
+    (void) umfpack_di_numeric (Ap, Ai, Ax, Symbolic, &Numeric, NULL, NULL) ;
+    umfpack_di_free_symbolic (&Symbolic) ;
+    (void) umfpack_di_solve (UMFPACK_A, Ap, Ai, Ax, x, rh, Numeric, NULL, NULL) ;
+    umfpack_di_free_numeric (&Numeric) ;
+
+    std::vector<double> v(x, x + dof);
+
+    delete [] x;
+
+    std::cout << "finish solving with UMFPACK\n" << std::endl;
+
+    return v;
+}
---
+++ UMFPACKSolver.h
@@ -0,0 +1,26 @@
+//
+//  UMFPACKSolver.h
+//  tri
+//
+//  Created by GBB on 21/2/15.
+//  Copyright (c) 2014 Xiaolin Guo. All rights reserved.
+//
+
+#ifndef __tri__UMFPACKSolver__
+#define __tri__UMFPACKSolver__
+
+#include "LinearSolver.h"
+#include <umfpack.h>
+
+class UMFPACKSolver: public LinearSolver
+{
+public:
+    UMFPACKSolver(std::vector< std::list<maColEle> > &ma,
+                  int femDof, double *femRH)
+        : LinearSolver(ma, femDof, femRH) {};
+
+    std::vector<double> solveSparse();  // call UMFPACK to solve the sparse linear system
+};
+
+
+#endif /* defined(__tri__UMFPACKSolver__) */
