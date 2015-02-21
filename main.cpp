145c37f6487bffe53b3adadd19c4081bb517a506
alpsberry <xiaolinguo.nju@gmail.com>
2015-02-21 14:35:39 +0100

    separate the sparse linear system solver with fem solving system

--- main.cpp
+++ main.cpp
@@ -1,19 +1,33 @@
-#include "tri_DG.h"
+//
+//  main.cpp
+//  tri
+//
+//  Created by GBB on 26/12/14.
+//  Copyright (c) 2014 Xiaolin Guo. All rights reserved.
+//
 
-int main(int argc, char const *argv[])
-{
+#include <iostream>
+#include "mesh.h"
+#include "BasicSolvingSystem.h"
+#include "DGSolvingSystem.h"
+#include "DGProblem.h"
+#include "problem.h"
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
     try {
         DGProblem prob(argc, argv);
-        Mesh mesh(prob);
-
-        DGSolvingSystem solSys;
-        solSys.assembleStiff(mesh, prob);
-        solSys.solveSparse(mesh, prob);
-        solSys.triOutput(mesh, prob);
-
+        Mesh mesh(&prob);
+        
+        BasicSolvingSystem* solSys = new DGSolvingSystem(&mesh, &prob);
+        solSys -> assembleStiff();
+        solSys -> solveSparse();
+        solSys -> output();
+        
     } catch (std::runtime_error &e) {
         cout << e.what() << endl;
     }
-
+    
     return 0;
 }
